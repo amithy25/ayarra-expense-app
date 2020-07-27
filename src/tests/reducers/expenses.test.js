e9c66decf78afa00c73f79178895ef54bdcc2cdb
Amith Y <amithy25@gmail.com>
2020-07-27 14:21:46 -0400

    get firebase expenses on start

--- src/tests/reducers/expenses.test.js
+++ src/tests/reducers/expenses.test.js
@@ -1,5 +1,6 @@
 import expensesReducer from "../../reducers/expenses";
 import expenses from "../fixtures/expenses";
+import createMockStore from "redux-mock-store";
 
 test("should set default state", () => {
   const state = expensesReducer(undefined, { type: "@@INIT" });
@@ -60,4 +61,13 @@ test("should not edit an expense if id not found", () => {
   };
   const state = expensesReducer(expenses, action);
   expect(state).toEqual(expenses);
+});
+
+test("should set expenses", () => {
+  const action = {
+    type: "SET_EXPENSES",
+    expenses: [expenses[1]],
+  };
+  const state = expensesReducer(expenses, action);
+  expect(state).toEqual([expenses[1]]);
 });
